ca5d6e4ebbc2d7d621efb3766489a1223db46e50
Chamud <chamudgeesara@gmail.com>
2023-03-01 18:17:56 +0530

    minor mod

--- distribution_system/settings.py
+++ distribution_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'goodsdistributionsystem-production.up.railway.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'goodsdistributionsystem-production.up.railway.app']
 
 
 # Application definition
