a3646321f8d750a1f5f5dd9df06b1d2e9ff14cff
Chamud <chamudgeesara@gmail.com>
2023-03-01 18:58:57 +0530

    remove media

--- distribution_system/settings.py
+++ distribution_system/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    #os.path.join(BASE_DIR, 'media'),
 ]
 
 # Default primary key field type
