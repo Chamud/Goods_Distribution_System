3313af042a828353ffabb167eee38fac235f60ac
Chamud <chamudgeesara@gmail.com>
2023-03-02 08:59:08 +0530

    Fix mime err

--- distribution_system/settings.py
+++ distribution_system/settings.py
@@ -16,10 +16,10 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#to fix mime type error
+# Add mime types for railway
 import mimetypes
-mimetypes.add_type("text/html", ".js", True)
-mimetypes.add_type("text/html", ".css", True)
+mimetypes.add_type("text/js", ".js", True)
+mimetypes.add_type("text/css", ".css", True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
