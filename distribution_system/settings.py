9cdab906b2f61f2db250be9ef2c16c201bc73c8a
Chamud <chamudgeesara@gmail.com>
2023-03-02 08:49:38 +0530

    minor fixes

--- distribution_system/settings.py
+++ distribution_system/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #bootsrat
+    #bootstrap
     'django_bootstrap_icons',
     #browser reload
     'django_browser_reload',
